6c30c9c82dd594e22c4e539dc47d63b092485544
ram5353 <ramkiran5353@gmail.com>
2019-12-12 07:43:29 +0530

    first commit

--- iAgree2.0/ViewController.swift
+++ iAgree2.0/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BottomTextView: UILabel!
+    @IBOutlet weak var TopTextView: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        BottomTextLineSpacing()
+        TopTextLineSpacing()
+        
     }
-
+    
+    func BottomTextLineSpacing() {
+        
+        let textForLabel = "Mit der neuen Historie-Funktion können Sie ab sofort Ihre in der Vergangenheit bearbeiteten Vorgänge einsehen."
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        //line height size
+        paragraphStyle.lineSpacing = 7
+        let attrString = NSMutableAttributedString(string: textForLabel)
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attrString.length))
+        BottomTextView.attributedText = attrString
+        BottomTextView.textAlignment = NSTextAlignment.left
+    }
+    
+    func TopTextLineSpacing() {
+        
+        let textForLabel1 = "Wir habben iAgree fur SIe neu aufgebaut!  Wishchen SIe nach links um ie Verbessrungen zu entdecken, die Ihnen die neue App biete"
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        //line height size
+        paragraphStyle.lineSpacing = 7
+        let attrString = NSMutableAttributedString(string: textForLabel1)
+        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attrString.length))
+        TopTextView.attributedText = attrString
+        TopTextView.textAlignment = NSTextAlignment.left
+    }
+    
+    
+    
 
 }
 
