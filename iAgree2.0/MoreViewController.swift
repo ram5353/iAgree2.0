5a536d6e53dca34d58e1a31fac5b2a3658746d9b
ram5353 <ramkiran5353@gmail.com>
2020-01-01 23:15:38 +0530

    Tab controller

---
+++ iAgree2.0/MoreViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MoreViewController.swift
+//  iAgree2.0
+//
+//  Created by ThinkServiceNew on 01/01/20.
+//  Copyright © 2020 vwits. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(true)
+        self.tabBarController?.navigationItem.title = "More"
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
